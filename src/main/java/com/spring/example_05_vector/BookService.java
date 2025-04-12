b9492bde4a08a11308c538b240ef8f3d3b2c598b
Michael Isvy <michael.isvy@gmail.com>
2025-04-12 08:36:15 +0800

    upgraded to Spring AI 1.0.0-M7

--- src/main/java/com/spring/example_05_vector/BookService.java
+++ src/main/java/com/spring/example_05_vector/BookService.java
@@ -1,7 +1,7 @@
 package com.spring.example_05_vector;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
